42cbc6e8d7cf81ce12aa33f3ba6b3aae1e6d0217
Arif6474 <arifulislam64743@gmail.com>
2022-04-18 06:32:42 +0600

    added personal info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import { Card } from 'react-bootstrap';
+import mypic from '../../images/mypic.jpg'
+import './About.css'
 const About = () => {
     return (
-        <div>
-            <h2>this is about</h2>
-        </div>
+        <Card style={{ width: '18rem' }} className="mx-auto mt-4 goal">
+  <Card.Img variant="top" src={mypic} />
+  <Card.Body>
+    <Card.Title className="text-center text-info">Md Ariful Islam</Card.Title>
+    <Card.Text className="text-secondary">
+      I want to be a front-end developer. For that , I am working hard and constantly learning. I want to learn more and more to be expert on programming. Insha-allah  I will success. 
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
     );
 };
 
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
+import "./Blog.css";
 
 const Blog = () => {
-    return (
-        <div>
-            <h2>This is a blog</h2>
-        </div>
-    );
+  return (
+    <div className="blog-container">
+      <div className="blog">
+        <h2 className="blog-title">
+          Difference between authorization and authentication?
+        </h2>
+        <p>
+          Authentication is the process of verifying someone or something to be
+          genuine or valid. In computing terms, this could be a person, process,
+          or device.Authorization is the process of permitting someone to access
+          something and specifying the type of access privileges and permissions
+          they have.
+        </p>
+      </div>
+      <div className="blog">
+        <h2 className="blog-title">
+          Why are we using firebase?What other options do we have to implement?
+        </h2>
+        <p>
+          The use of Firebase in building mobile and web applications is common
+          nowadays. Developers rely on this platform because of extensive tools,
+          fast development, and real-time databases to make interactive apps.
+          Crash reporting, Google Analytics, secure hosting and authentication
+          are also benefits of using Firebase.
+        </p>
+      </div>
+      <div className="blog">
+        <h2 className="blog-title">
+          What others services does firebase provide?
+        </h2>
+        <p>
+          Cloud Firestore, Cloud Functions,Hosting, Cloud
+          Storage, Google Analytics, Predictions, Cloud Messaging.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
